8e5dc674a6b44ae330edf5e562d6ac44867975c7
Paul Carvill <paul@paulcarvill.com>
2013-06-11 00:51:54 +0100

    trying different Capistrano + Nginx + Unicorn setup/config

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,65 +1,28 @@
-# config/unicorn.rb
-# Set environment to development unless something else is specified
-env = ENV["RAILS_ENV"] || "development"
+# define paths and filenames
+deploy_to = "/srv/net"
+rack_root = "#{deploy_to}/current"
+pid_file = "#{deploy_to}/shared/pids/unicorn.pid"
+socket_file= "#{deploy_to}/shared/unicorn.sock"
+log_file = "#{rack_root}/log/unicorn.log"
+err_log = "#{rack_root}/log/unicorn_error.log"
+old_pid = pid_file + '.oldbin'
 
-# See http://unicorn.bogomips.org/Unicorn/Configurator.html for complete
-# documentation.
-worker_processes 4
-
-# listen on both a Unix domain socket and a TCP port,
-# we use a shorter backlog for quicker failover when busy
-listen "/tmp/piBoard.socket", :backlog => 64
-
-# Preload our app for more speed
-preload_app true
-
-# nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
+worker_processes 2
+listen socket_file, :backlog => 1024
 
-pid "/home/deployer/apps/piBoard/shared/tmp/pids/unicorn.piBoard.pid"
-
-# Production specific settings
-if env == "production"
-  # Help ensure your application will always spawn in the symlinked
-  # "current" directory that Capistrano sets up.
-  working_directory "/home/deployer/apps/piBoard/current"
-
-  # feel free to point this anywhere accessible on the filesystem
-  user 'deployer', 'staff'
-  shared_path = "/home/deployer/apps/piBoard/shared"
-
-  stderr_path "#{shared_path}/log/unicorn.stderr.log"
-  stdout_path "#{shared_path}/log/unicorn.stdout.log"
-end
+pid pid_file
+stderr_path err_log
+stdout_path log_file
 
 before_fork do |server, worker|
-  # the following is highly recomended for Rails + "preload_app true"
-  # as there's no need for the master process to hold a connection
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-  end
-
-  # Before forking, kill the master process that belongs to the .oldbin PID.
-  # This enables 0 downtime deploys.
-  old_pid = "/home/deployer/apps/piBoard/shared/tmp/pids/unicorn.piBoard.pid.oldbin"
+  # zero downtime deploy magic:
+  # if unicorn is already running, ask it to start a new process and quit.
   if File.exists?(old_pid) && server.pid != old_pid
     begin
       Process.kill("QUIT", File.read(old_pid).to_i)
     rescue Errno::ENOENT, Errno::ESRCH
-      # someone else did our job for us
+      # already done
     end
   end
-end
-
-after_fork do |server, worker|
-  # the following is *required* for Rails + "preload_app true",
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-  end
-
-  # if preload_app is true, then you may also want to check and
-  # restart any other shared sockets/descriptors such as Memcached,
-  # and Redis.  TokyoCabinet file handles are safe to reuse
-  # between any number of forked children (assuming your kernel
-  # correctly implements pread()/pwrite() system calls)
 end
